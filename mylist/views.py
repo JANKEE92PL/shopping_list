bd9eaf6697b28a0080122ee2a5af48dae537dcfa
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-06-28 14:58:51 +0200

    fetching data from frontend to backend

--- mylist/views.py
+++ mylist/views.py
@@ -1,6 +1,11 @@
+from unicodedata import name
 from django.shortcuts import render
+from .models import ShoppingItem
 
 # Create your views here.
 
 def mylist(request):
+    if request.method == 'POST':
+        print('Received data:' , request.POST['itemName'])
+        ShoppingItem.objects.create(name= request.POST['itemName'])
     return render(request, 'shopping_list.html')
