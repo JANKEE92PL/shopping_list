e9eb8210263b039967aeda0d72b3cd51157536f0
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-06-28 14:14:47 +0200

    creating view shopping_list.html

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mylist.views import mylist
+
+from mylist.views import mylist
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mylist/', ),
+    path('mylist/', mylist),
 ]
