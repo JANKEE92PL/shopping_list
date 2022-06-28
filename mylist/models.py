58195c84a035042b41cc897613bf01e3acf08b40
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-06-28 15:18:56 +0200

    finish admin page

--- mylist/models.py
+++ mylist/models.py
@@ -7,3 +7,6 @@ class ShoppingItem(models.Model):
     created_at = models.DateField(default=date.today)
     name = models.CharField(max_length=200)
     done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
